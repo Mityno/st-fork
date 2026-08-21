9b4de4f94099da6ae49e7b40d518222367188e47
Mityno <pititcowboy@gmail.com>
2026-08-21 14:34:56 +0200

    feat: Start decompiling MapObjectUnkLTRW

--- include/MapObject/MapObjectUnkLTRW.hpp
+++ include/MapObject/MapObjectUnkLTRW.hpp
@@ -4,13 +4,28 @@
 
 #include "MapObject/MapObject.hpp"
 #include "MapObject/MapObjectProfile.hpp"
+#include "Render/ModelRender.hpp"
 #include "global.h"
-#include "types.h"
+
+class UnkStruct_ov063_021639e4 {
+public:
+    /* 00 */
+
+    UnkStruct_ov063_021639e4() {};
+
+    /* 04 */ virtual void vfunc_04() override;
+};
 
 class MapObjectUnkLTRW : public MapObject {
 public:
     /* 00 (base) */
-    /* 40 */
+    /* 40 */ ModelRender mUnk_40;
+    /* A0 */ unk32 mUnk_A0;
+    /* A4 */ unk16 mUnk_A4;
+    /* A6 */ unk16 mUnk_A6;
+    /* A8 */ UnkStruct_ov063_021639e4 mUnk_A8;
+    /* AC */ unk16 mUnk_AC; // Both members are probably part of mUnk_A8
+    /* AE */ unk16 mUnk_AE;
 
     MapObjectUnkLTRW();
 
@@ -25,7 +40,22 @@ public:
     void func_ov063_02160e18(void);
 };
 
-class MapObjectProfileUnkLTRW : public MapObjectProfile {
+class MapObjectProfileUnkLTRW_Base_D4 : public MapObject_10 {
+public:
+};
+
+class MapObjectProfileUnkLTRW_Base : public MapObjectProfile_Derived2 {
+public:
+    /* 00 (base) */
+    /* D4 */ MapObjectProfileUnkLTRW_Base_D4 mUnk_D4;
+    /* F8 */
+
+    MapObjectProfileUnkLTRW_Base(MapObjectId mapObjId1, MapObjectId mapObjId2) :
+        MapObjectProfile_Derived2(mapObjId1, mapObjId2),
+        mUnk_D4() {};
+};
+
+class MapObjectProfileUnkLTRW : public MapObjectProfileUnkLTRW_Base {
 public:
     /* 00 (base) */
 
--- src/063_Shrine/MapObject/MapObjectUnkLTRW.cpp
+++ src/063_Shrine/MapObject/MapObjectUnkLTRW.cpp
@@ -1,18 +1,44 @@
-//! TODO: This file was generated automatically and might contain errors
-
 #include "MapObject/MapObjectUnkLTRW.hpp"
+#include "MapObject/MapObjectProfile.hpp"
 #include "System/SysNew.hpp"
+#include "global.h"
+#include "nitro/fx.h"
+#include "nitro/math.h"
+#include "nns/g3d/g3d.h"
+#include "profile.hpp"
+
+struct UnkStruct_data_ov063_021639c4 {
+    /* 00 */ STRUCT_PAD(0x0, 0x0C);
+    /* 0C */ VecFx32 vec;
+};
+
+extern UnkStruct_data_ov063_021639c4 data_ov063_021639c4;
 
 DECL_PROFILE(MapObjectProfileUnkLTRW);
 
 MapObject *MapObjectProfileUnkLTRW::Create() {
-    return new(HeapIndex_2) MapObjectUnkLTRW();
+    return new(HeapIndex_ITCM) MapObjectUnkLTRW();
 }
 
 MapObjectProfileUnkLTRW::MapObjectProfileUnkLTRW() :
-    MapObjectProfile(MapObjectId_LTRW) {}
+    MapObjectProfileUnkLTRW_Base(MapObjectId_LTRW, MapObjectId_LTRW) {
+    mUnk_D4.mUnk_08 = 0x04007007;
+    VecFx32_Init(data_ov063_021639c4.vec.x - 0x99A, data_ov063_021639c4.vec.y, data_ov063_021639c4.vec.z - 0x666,
+                 &mUnk_D4.mUnk_0C);
+    VecFx32_Init(data_ov063_021639c4.vec.x + 0x99A, data_ov063_021639c4.vec.y + 0x1333, data_ov063_021639c4.vec.z + 0x666,
+                 &mUnk_D4.mUnk_18);
+    mUnk_06 = 1;
+    mUnk_0C = 0x1333;
+}
 
-MapObjectUnkLTRW::MapObjectUnkLTRW() {}
+MapObjectUnkLTRW::MapObjectUnkLTRW() :
+    mUnk_40(G3d_GetModelPtr(GET_PROFILE(MapObjectProfileUnkLTRW)->mUnk_20.mUnk_50)),
+    mUnk_A0(1),
+    mUnk_A4(0),
+    mUnk_A6(0),
+    mUnk_A8(),
+    mUnk_AC(0),
+    mUnk_AE(0) {}
 
 void MapObjectUnkLTRW::func_ov063_02160c6c(void) {}
 void MapObjectUnkLTRW::func_ov063_02160cc8(void) {}
